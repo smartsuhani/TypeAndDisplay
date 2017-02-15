c321e4db42f0178abb43f20c94af3902d42908cf
smartsuhani <lanetteam.suhani@gmail.com>
2017-02-15 11:47:24 +0530

    app flow arrangement

--- TypeAndDisplay/TypeAndDisplay/FifthViewController.swift
+++ TypeAndDisplay/TypeAndDisplay/FifthViewController.swift
@@ -2,8 +2,14 @@ import UIKit
 
 class FifthViewController: UITabBarController,UITabBarControllerDelegate {
 
-    var tab1: ThirdViewController!
-    var tab2: FourthViewController!
+    var tab1: RootViewController!
+    var tab2: NinthViewController!
+    var tab3: SixViewController!
+    var tab4: SeventhViewController!
+    var tab5: EightViewController!
+    var tab6: MapViewController!
+    var tab7: RegisterViewController!
+    var tab8: WebViewController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -11,16 +17,35 @@ class FifthViewController: UITabBarController,UITabBarControllerDelegate {
         // Do any additional setup after loading the view.
     }
     override func viewWillAppear(_ animated: Bool) {
-        tab1 = ThirdViewController()
-        tab1.tabBarItem = UITabBarItem(title: "Images", image: UIImage(named: "imageicon"), selectedImage: UIImage(named: "imageicon"))
+        tab1 = RootViewController()
+        tab1.tabBarItem = UITabBarItem(title: "splitview", image: UIImage(named: "split"), selectedImage: UIImage(named: "split"))
         
-        tab2 = FourthViewController()
-        tab2.tabBarItem = UITabBarItem(title: "User", image: UIImage(named: "usericon"), selectedImage: UIImage(named: "usericon"))
+        tab2 = NinthViewController()
+        tab2.tabBarItem = UITabBarItem(title: "animation", image: UIImage(named: "animation"), selectedImage: UIImage(named: "animation"))
         
-        self.viewControllers = [tab1,tab2]
+        tab3 = SixViewController()
+        tab3.tabBarItem = UITabBarItem(title: "file", image: UIImage(named: "file"), selectedImage: UIImage(named: "file"))
+        
+        tab4 = SeventhViewController()
+        tab4.tabBarItem = UITabBarItem(title: "page", image: UIImage(named: "page"), selectedImage: UIImage(named: "page"))
+        
+        tab5 = EightViewController()
+        tab5.tabBarItem = UITabBarItem(title: "scroll", image: UIImage(named: "scroll"), selectedImage: UIImage(named: "scroll"))
+        
+        tab6 = MapViewController()
+        tab6.tabBarItem = UITabBarItem(title: "map", image: UIImage(named: "map"), selectedImage: UIImage(named: "map"))
+        
+        tab7 = RegisterViewController()
+        tab7.tabBarItem = UITabBarItem(title: "register", image: UIImage(named: "register"), selectedImage: UIImage(named: "register"))
+        
+        tab8 = WebViewController()
+        tab8.tabBarItem = UITabBarItem(title: "web", image: UIImage(named: "web"), selectedImage: UIImage(named: "web"))
+        
+        self.viewControllers = [tab3,tab4,tab5,tab2,tab1,tab6,tab7,tab8]
 
 
-    }    
+    }
+
     // MARK: - Navigation
 
 }
